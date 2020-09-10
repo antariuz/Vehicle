35d34f8d9e6db70691fae7b5d9d1f043a63e1b16
Tymur Foshch <antariuz@gmail.com>
2020-09-10 22:34:21 +0300

    Done MyComparator class

--- src/best/com/MyComparator.java
+++ src/best/com/MyComparator.java
@@ -9,16 +9,29 @@ public class MyComparator implements Comparator<Vehicle> {
     public int compare(Vehicle o1, Vehicle o2) {
 
         Iterator<Passenger> iteratorForo1 = o1.getPassengers().iterator();
-        Passenger olderPassengerInVehicleO1 = o1.getPassengers().iterator().next();
-        while (iteratorForo1.hasNext()) {
-            olderPassengerInVehicleO1 = olderPassengerInVehicleO1.getAge() > o1.getPassengers().iterator().next().getAge()
-                    ? olderPassengerInVehicleO1 : o1.getPassengers().iterator().next();
+        Passenger olderPassengerInVehicleO1 = iteratorForo1.next();
+        if (iteratorForo1.hasNext()) {
+            Passenger olderPassengerInVehicleO1Next = iteratorForo1.next();
+            while (iteratorForo1.hasNext()) {
+                olderPassengerInVehicleO1 = olderPassengerInVehicleO1.getAge() > olderPassengerInVehicleO1Next.getAge()
+                        ? olderPassengerInVehicleO1 : olderPassengerInVehicleO1Next;
+                olderPassengerInVehicleO1Next = iteratorForo1.next();
+            }
+            olderPassengerInVehicleO1 = olderPassengerInVehicleO1.getAge() > olderPassengerInVehicleO1Next.getAge()
+                    ? olderPassengerInVehicleO1 : olderPassengerInVehicleO1Next;
         }
+
         Iterator<Passenger> iteratorForo2 = o2.getPassengers().iterator();
-        Passenger olderPassengerInVehicleO2 = o2.getPassengers().iterator().next();
-        while (iteratorForo2.hasNext()){
-            olderPassengerInVehicleO2 = olderPassengerInVehicleO2.getAge() > o2.getPassengers().iterator().next().getAge()
-                    ? olderPassengerInVehicleO2 : o2.getPassengers().iterator().next();
+        Passenger olderPassengerInVehicleO2 = iteratorForo2.next();
+        if (iteratorForo2.hasNext()) {
+            Passenger olderPassengerInVehicleO2Next = iteratorForo2.next();
+            while (iteratorForo2.hasNext()){
+                olderPassengerInVehicleO2 = olderPassengerInVehicleO2.getAge() > olderPassengerInVehicleO2Next.getAge()
+                        ? olderPassengerInVehicleO2 : olderPassengerInVehicleO2Next;
+                olderPassengerInVehicleO2Next = iteratorForo2.next();
+            }
+            olderPassengerInVehicleO2 = olderPassengerInVehicleO2.getAge() > olderPassengerInVehicleO2Next.getAge()
+                    ? olderPassengerInVehicleO2 : olderPassengerInVehicleO2Next;
         }
 
         return olderPassengerInVehicleO2.getAge().compareTo(olderPassengerInVehicleO1.getAge());
